b1bf874c715f19130faed540045265ce9ff4058c
proteinbor <114232015+proteinbor@users.noreply.github.com>
2023-05-30 10:24:59 +0300

    fixed title typo

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Scooby',
+  title: 'Scoobi',
   description: 'Rent a bike!',
 }
 
